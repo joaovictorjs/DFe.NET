bde7f08a057c6c03791cab57ff857e3eb1756e67
joaovictorjs <skai7ami@gmail.com>
2026-06-24 19:54:42 -0300

    feat: cria flyout de configurações

---
+++ MDFe.AppTeste2/Views/ConfiguracoesView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace MDFe.AppTeste2.Views
+{
+    /// <summary>
+    /// Interaction logic for ConfiguracoesView.xaml
+    /// </summary>
+    public partial class ConfiguracoesView : Flyout
+    {
+        public ConfiguracoesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
