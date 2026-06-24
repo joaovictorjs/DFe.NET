0625d14dc34c71082a8c910588500f953ab20c7b
joaovictorjs <skai7ami@gmail.com>
2026-06-24 19:11:30 -0300

    feat: adiciona dryioc

--- MDFe.AppTeste2/App.xaml.cs
+++ MDFe.AppTeste2/App.xaml.cs
@@ -1,6 +1,5 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using DryIoc;
 
 namespace MDFe.AppTeste2
 {
@@ -9,6 +8,16 @@ namespace MDFe.AppTeste2
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            var container = new Container();
+
+            container.Register<MainWindow>();
+
+            var janelaPrincipal = container.Resolve<MainWindow>();
+            janelaPrincipal.Show();
+        }
+    }
 }
